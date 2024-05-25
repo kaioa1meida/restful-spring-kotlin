fcb241537bb3f3b1042931ea0438c549578cc528
Kaio Almeida <kaiohsilva04@gmail.com>
2024-05-24 23:46:17 -0300

    feat: sending initial application model

---
+++ src/main/kotlin/org/starcode/Greeting.kt
@@ -0,0 +1,3 @@
+package org.starcode
+
+class Greeting(val id: Long, val content: String)
---
+++ src/test/kotlin/org/starcode/StartupTest.kt
@@ -0,0 +1,13 @@
+package org.starcode
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartupTest {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
