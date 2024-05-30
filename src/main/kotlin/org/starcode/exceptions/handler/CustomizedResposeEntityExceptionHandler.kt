53df2c00888222ca4bf1e28cfca4c3a3e8078b74
Kaio Almeida <kaiohsilva04@gmail.com>
2024-05-29 21:25:18 -0300

    feat: setting exceptions

---
+++ src/main/kotlin/org/starcode/exceptions/handler/CustomizedResposeEntityExceptionHandler.kt
@@ -0,0 +1,39 @@
+package org.starcode.exceptions.handler
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import org.starcode.exceptions.ExceptionResponse
+import org.starcode.exceptions.ResourceNotFoundException
+import java.util.*
+import java.lang.*
+
+@ControllerAdvice
+@RestController
+class CustomizedResposeEntityExceptionHandler: ResponseEntityExceptionHandler(){
+
+    @ExceptionHandler(Exception::class)
+    fun handleAllExceptions(ex: Exception, request: WebRequest): ResponseEntity<ExceptionResponse>{
+        val exceptionResponse = ExceptionResponse(
+            Date(),
+            ex.message,
+            request.getDescription(false)
+        )
+        return ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @ExceptionHandler(ResourceNotFoundException::class)
+    fun handleResourceNotFoundException(ex: Exception, request: WebRequest): ResponseEntity<ExceptionResponse>{
+        val exceptionResponse = ExceptionResponse(
+            Date(),
+            ex.message,
+            request.getDescription(false)
+        )
+        return ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.NOT_FOUND)
+    }
+
+}
