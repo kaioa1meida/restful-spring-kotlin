421626a4a6cdcfcdd627155814a35d7b38ccc867
Kaio Almeida <kaiohsilva04@gmail.com>
2024-05-29 21:27:36 -0300

    feat: adding VO project pattern

---
+++ src/main/kotlin/org/starcode/data/vo/PersonVO.kt
@@ -0,0 +1,11 @@
+package org.starcode.data.vo
+
+data class PersonVO(
+
+    var id: Long = 0,
+    var firstName: String = "",
+    var lastName: String = "",
+    var address: String = "",
+    var gender: String = ""
+
+)
