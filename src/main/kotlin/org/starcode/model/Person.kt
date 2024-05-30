9060789461295301fceb17284ea6b30dda02cac3
Kaio Almeida <kaiohsilva04@gmail.com>
2024-05-29 21:28:35 -0300

    feat: adding Person entity

---
+++ src/main/kotlin/org/starcode/model/Person.kt
@@ -0,0 +1,21 @@
+package org.starcode.model
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "person")
+data class Person(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+
+    @Column(name = "first_name", nullable = false)
+    var firstName: String = "",
+    @Column(name = "last_name", nullable = false)
+    var lastName: String = "",
+    @Column(nullable = false)
+    var address: String = "",
+    @Column(nullable = false)
+    var gender: String = ""
+)
