9e3b8cfabdc270e58229c68ff39ff701b3c28a16
Kaio Almeida <kaiohsilva04@gmail.com>
2024-07-06 16:18:17 -0300

    feat: add CORS configuration

--- src/main/kotlin/org/starcode/config/WebConfig.kt
+++ src/main/kotlin/org/starcode/config/WebConfig.kt
@@ -1,9 +1,11 @@
 package org.starcode.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
 import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.starcode.serialization.converter.YamlJackson2HttpMessageConverter
 
@@ -11,6 +13,9 @@ import org.starcode.serialization.converter.YamlJackson2HttpMessageConverter
 class WebConfig : WebMvcConfigurer {
 
     private val MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml")
+
+    @Value("\${cors.originPatterns:default}")
+    private val corsOriginPatterns: String = ""
 
     override fun extendMessageConverters(converters: MutableList<HttpMessageConverter<*>>) {
         converters.add(YamlJackson2HttpMessageConverter())
@@ -25,6 +30,15 @@ class WebConfig : WebMvcConfigurer {
                 .mediaType("json", MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML)
                 .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML)
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        val allowedOrigins = corsOriginPatterns.split(",").toTypedArray()
+
+        registry.addMapping("/**")
+            .allowedMethods("GET","POST","PUT","PATCH","DELETE")
+            .allowedOrigins(*allowedOrigins)
+            .allowCredentials(true)
     }
 
 }
