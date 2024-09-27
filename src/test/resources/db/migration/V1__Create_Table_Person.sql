7eeeb3610ad9089b4f47259ae366f7620a068c58
Kaio Almeida <kaiohsilva04@gmail.com>
2024-09-27 16:51:28 -0300

    refactor: remove unused arquives

--- src/test/resources/db/migration/V1__Create_Table_Person.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS person (
-   id BIGINT AUTO_INCREMENT NOT NULL,
-   first_name VARCHAR(255) NOT NULL,
-   last_name VARCHAR(255) NOT NULL,
-   address VARCHAR(255) NOT NULL,
-   gender VARCHAR(255) NOT NULL,
-   PRIMARY KEY (id)
-);
