a7280d73ce44c8fd226ed66f738f50c72382ae37
Kaio Almeida <kaiohsilva04@gmail.com>
2024-09-27 16:28:20 -0300

    test: adding Authentication header in the tests requests

--- src/test/kotlin/org/starcode/integrationtests/controller.cors.withjson/PersonControllerCorsWithJson.kt
+++ src/test/kotlin/org/starcode/integrationtests/controller.cors.withjson/PersonControllerCorsWithJson.kt
@@ -1,8 +1,11 @@
 package org.starcode.integrationtests.controller.cors.withjson
 
+import org.starcode.integrationtests.testcontainers.AbstractIntegrationTest
+import org.starcode.integrationtests.vo.AccountCredentialsVO
+import org.starcode.integrationtests.vo.PersonVO
+import org.starcode.integrationtests.vo.TokenVO
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
-import io.restassured.RestAssured
 import io.restassured.RestAssured.given
 import io.restassured.builder.RequestSpecBuilder
 import io.restassured.filter.log.LogDetail
@@ -11,25 +14,51 @@ import io.restassured.filter.log.ResponseLoggingFilter
 import io.restassured.specification.RequestSpecification
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.TestInstance.Lifecycle
 import org.springframework.boot.test.context.SpringBootTest
-import org.starcode.integrationtests.vo.PersonVO
 import org.starcode.integrationtests.TestConfigs
-import org.starcode.integrationtests.testcontainers.AbstractIntegrationTest
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestInstance(Lifecycle.PER_CLASS)
 class PersonControllerCorsWithJson() : AbstractIntegrationTest() {
 
 	private lateinit var specification: RequestSpecification
 	private lateinit var objectMapper: ObjectMapper
 	private lateinit var person: PersonVO
 
+	private lateinit var token: String
+
 	@BeforeAll
 	fun setupTests(){
 		objectMapper = ObjectMapper()
 		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
 		person = PersonVO()
+		token = ""
+	}
+
+	@Test
+	@Order(0)
+	fun authorization() {   // Gambiarra Galática
+		val user = AccountCredentialsVO(
+			username = "teste",
+			password = "teste"
+		)
+
+		token = given()
+			.basePath("/auth/signin")
+			.port(TestConfigs.SERVER_PORT)
+			.contentType(TestConfigs.CONTENT_TYPE_JSON)
+			.body(user)
+			.`when`()
+			.post()
+			.then()
+			.assertThat()
+			.statusCode(200)
+			.extract()
+			.jsonPath()
+			.getString("accessToken")
+
 	}
 
 	@Test
@@ -41,6 +70,10 @@ class PersonControllerCorsWithJson() : AbstractIntegrationTest() {
 			.addHeader(
 				TestConfigs.HEADER_PARAM_ORIGIN,
 				TestConfigs.ORIGIN_LOCALHOST
+			)
+			.addHeader(
+				TestConfigs.HEADER_PARAM_AUTHORIZATION,
+				"Bearer $token"
 			)
 			.setBasePath("/api/v1/person")
 			.setPort(TestConfigs.SERVER_PORT)
@@ -90,7 +123,11 @@ class PersonControllerCorsWithJson() : AbstractIntegrationTest() {
 				TestConfigs.HEADER_PARAM_ORIGIN,
 				TestConfigs.ORIGIN_IBM
 			)
-			.setBasePath("/api/v1/person")
+			.addHeader(
+				TestConfigs.HEADER_PARAM_AUTHORIZATION,
+				"Bearer $token"
+			)
+			.setBasePath("/api/person/v1")
 			.setPort(TestConfigs.SERVER_PORT)
 			.addFilter(RequestLoggingFilter(LogDetail.ALL))
 			.addFilter(ResponseLoggingFilter(LogDetail.ALL))
@@ -120,6 +157,10 @@ class PersonControllerCorsWithJson() : AbstractIntegrationTest() {
 			.addHeader(
 				TestConfigs.HEADER_PARAM_ORIGIN,
 				TestConfigs.ORIGIN_LOCALHOST
+			)
+			.addHeader(
+				TestConfigs.HEADER_PARAM_AUTHORIZATION,
+				"Bearer $token"
 			)
 			.setBasePath("/api/v1/person")
 			.setPort(TestConfigs.SERVER_PORT)
@@ -166,7 +207,11 @@ class PersonControllerCorsWithJson() : AbstractIntegrationTest() {
 				TestConfigs.HEADER_PARAM_ORIGIN,
 				TestConfigs.ORIGIN_IBM
 			)
-			.setBasePath("/api/v1/person")
+			.addHeader(
+				TestConfigs.HEADER_PARAM_AUTHORIZATION,
+				"Bearer $token"
+			)
+			.setBasePath("/api/person/v1")
 			.setPort(TestConfigs.SERVER_PORT)
 			.addFilter(RequestLoggingFilter(LogDetail.ALL))
 			.addFilter(ResponseLoggingFilter(LogDetail.ALL))
@@ -193,182 +238,3 @@ class PersonControllerCorsWithJson() : AbstractIntegrationTest() {
 		person.gender = "Male"
 	}
 }
-
-
-/*
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@TestMethodOrder(MethodOrderer.OrderAnnotation::class) // apenas em testes de integração
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class PersonControllerCorsWithJson() : AbstractIntegrationTest() {
-
-	private lateinit var specification: RequestSpecification
-	private lateinit var objectMapper: ObjectMapper
-	private lateinit var person: PersonVO
-
-	@BeforeAll
-	fun setupTests(){
-		objectMapper = ObjectMapper()
-		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-		person = PersonVO()
-	}
-
-	@Test
-	@Order(1)
-	fun testCreate() {
-		mockPerson()
-
-		specification = RequestSpecBuilder()
-			.addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_LOCALHOST)
-			.setBasePath("/api/v1/person")
-			.setPort(TestConfigs.SERVER_PORT)
-			.addFilter(RequestLoggingFilter(LogDetail.ALL))
-			.addFilter(ResponseLoggingFilter(LogDetail.ALL))
-			.build()
-
-		val content = RestAssured.given()
-			.spec(specification)
-			.contentType(TestConfigs.CONTENT_TYPE_JSON)
-			.body(person)
-				.`when`()
-			.post()
-			.then()
-				.statusCode(200)
-			.extract()
-			.body()
-			.asString()
-
-		val createdPerson = objectMapper.readValue(
-			content,
-			PersonVO::class.java
-		)
-		person = createdPerson
-
-		assertNotNull(createdPerson.id)
-		assertNotNull(createdPerson.firstName)
-		assertNotNull(createdPerson.lastName)
-		assertNotNull(createdPerson.address)
-		assertNotNull(createdPerson.gender)
-
-		assertTrue(createdPerson.id > 0)
-
-		assertEquals("Nelson", createdPerson.firstName)
-		assertEquals("Piquet", createdPerson.lastName)
-		assertEquals("Brasília, DF, Brasil", createdPerson.address)
-		assertEquals("Male", createdPerson.gender)
-	}
-
-	@Test
-	@Order(2)
-	fun testCreateWithWrongOrigin() {
-		mockPerson()
-
-		specification = RequestSpecBuilder()
-			.addHeader(
-				TestConfigs.HEADER_PARAM_ORIGIN,
-				TestConfigs.ORIGIN_IBM
-			)
-			.setBasePath("/api/v1/person")
-			.setPort(TestConfigs.SERVER_PORT)
-			.addFilter(RequestLoggingFilter(LogDetail.ALL))
-			.addFilter(ResponseLoggingFilter(LogDetail.ALL))
-			.build()
-
-		val content = RestAssured.given()
-			.spec(specification)
-			.contentType(TestConfigs.CONTENT_TYPE_JSON)
-			.body(person)
-			.`when`()
-			.post()
-			.then()
-			.statusCode(403)
-			.extract()
-			.body()
-			.asString()
-
-		assertEquals("Invalid CORS request", content)
-	}
-
-	@Test
-	@Order(3)
-	fun testFindById() {
-		mockPerson()
-
-		specification = RequestSpecBuilder()
-			.addHeader(
-				TestConfigs.HEADER_PARAM_ORIGIN,
-				TestConfigs.ORIGIN_LOCALHOST
-			)
-			.setBasePath("/api/v1/person")
-			.setPort(TestConfigs.SERVER_PORT)
-			.addFilter(RequestLoggingFilter(LogDetail.ALL))
-			.addFilter(ResponseLoggingFilter(LogDetail.ALL))
-			.build()
-
-		val content = RestAssured.given()
-			.spec(specification)
-			.contentType(TestConfigs.CONTENT_TYPE_JSON)
-			.pathParam("id", person.id)
-			.`when`()["{id}"]
-			.then()
-			.statusCode(200)
-			.extract()
-			.body()
-			.asString()
-
-		val createdPerson = objectMapper.readValue(
-			content,
-			PersonVO::class.java
-		)
-		assertNotNull(createdPerson.id)
-		assertNotNull(createdPerson.firstName)
-		assertNotNull(createdPerson.lastName)
-		assertNotNull(createdPerson.address)
-		assertNotNull(createdPerson.gender)
-
-		assertTrue(createdPerson.id > 0)
-
-		assertEquals("Nelson", createdPerson.firstName)
-		assertEquals("Piquet", createdPerson.lastName)
-		assertEquals("Brasília, DF, Brasil", createdPerson.address)
-		assertEquals("Male", createdPerson.gender)
-	}
-
-	@Test
-	@Order(4)
-	fun testFindByIdWithWrongOrigin() {
-		mockPerson()
-
-		specification = RequestSpecBuilder()
-			.addHeader(
-				TestConfigs.HEADER_PARAM_ORIGIN,
-				TestConfigs.ORIGIN_IBM
-			)
-			.setBasePath("/api/v1/person")
-			.setPort(TestConfigs.SERVER_PORT)
-			.addFilter(RequestLoggingFilter(LogDetail.ALL))
-			.addFilter(ResponseLoggingFilter(LogDetail.ALL))
-			.build()
-
-		val content = RestAssured.given()
-			.spec(specification)
-			.contentType(TestConfigs.CONTENT_TYPE_JSON)
-			.pathParam("id", person.id)
-			.`when`()["{id}"]
-			.then()
-			.statusCode(403)
-			.extract()
-			.body()
-			.asString()
-
-		assertEquals("Invalid CORS request", content)
-	}
-
-	private fun mockPerson() {
-		person.firstName = "Nelson"
-		person.lastName = "Piquet"
-		person.address = "Brasília, DF, Brasil"
-		person.gender = "Male"
-	}
-
-}
-*/
