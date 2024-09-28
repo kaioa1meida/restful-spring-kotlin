5ba97ff7d1ff5f0d1a6af7e23ebad43bf22e86fe
kaioa1meida <kaiohsilva04@gmail.com>
2024-09-28 18:33:44 -0300

    refactor: modify passwd

--- src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
+++ src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -1,4 +1,4 @@
 ﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
-	('kaio.almeida', 'Kaio Almeida', '19bbf735b27066f2f145e602624e1b24a3fbc54cd5dfd3143fc5feea6bdee9e139ca7332d4806b9f', b'1', b'1', b'1', b'1'),
+	('kaio.almeida', 'Kaio Almeida', 'b49aa0c0276f7a28a774355161c9346cef14013d8ed1d991b2a3e31e318672249eb1c6cc39949bd2', b'1', b'1', b'1', b'1'),
 	('jorge.motta', 'Jorge Motta', '75ec349c1b0ef4ee7b249d0b83ae4861853f3aa77bce8c4b15f28cd43c6424ab4f29df431831bb0d', b'1', b'1', b'1', b'1'),
 	('teste', 'Teste', 'eb273690ed1fc0ec8ae86980b927888b667b6127910ebd6b2bba12155d411733f22fafdf8e0ac1f1', b'1', b'1', b'1', b'1');
