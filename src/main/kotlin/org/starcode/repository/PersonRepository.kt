04c8f176b8665a1667cff7b7ef8092f9793731fc
kaioa1meida <kaiohsilva04@gmail.com>
2024-10-03 10:57:54 -0300

    feat: adding new Query Method to change enabled atribute

--- src/main/kotlin/org/starcode/repository/PersonRepository.kt
+++ src/main/kotlin/org/starcode/repository/PersonRepository.kt
@@ -1,8 +1,16 @@
 package org.starcode.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import org.starcode.model.Person
 
 @Repository
-interface PersonRepository: JpaRepository<Person, Long?> {}
+interface PersonRepository: JpaRepository<Person, Long?> {
+
+    @Modifying
+    @Query("UPDATE Person p SET p.enabled = false WHERE p.id =:id")
+    fun disablePerson(@Param("id") id: Long?)
+}
