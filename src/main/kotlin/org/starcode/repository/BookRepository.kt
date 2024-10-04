4199b9965ceb2f82b85037ef74097def7f2397f4
kaioa1meida <kaiohsilva04@gmail.com>
2024-10-04 10:26:51 -0300

    feat: add new method and Pagination for Books

--- src/main/kotlin/org/starcode/repository/BookRepository.kt
+++ src/main/kotlin/org/starcode/repository/BookRepository.kt
@@ -1,8 +1,17 @@
 package org.starcode.repository
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import org.starcode.model.Book
+import org.starcode.model.Person
 
 @Repository
-interface BookRepository: JpaRepository<Book, Long>
+interface BookRepository: JpaRepository<Book, Long> {
+
+    @Query("SELECT b FROM Book b WHERE b.title LIKE LOWER(CONCAT ('%',:title,'%'))")
+    fun findBookByTitle(@Param("title") title: String, pageable: Pageable) : Page<Book>
+}
