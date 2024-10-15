520501fe3bfe67d4cabcc886ab7fc47bd21f75e5
kaioa1meida <kaiohsilva04@gmail.com>
2024-10-15 01:40:13 -0300

    test: adjusting gambiarras hehe

--- src/test/kotlin/org/starcode/unittests/mockito/services/BookServiceTest.kt
+++ src/test/kotlin/org/starcode/unittests/mockito/services/BookServiceTest.kt
@@ -71,7 +71,7 @@ class BookServiceTest {
         assertEquals("Title Test1",result.title)
         assertEquals("Author Test1",result.author)
         assertEquals(1.0,result.price)
-        assertEquals("Thu Feb 01 00:00:00 UTC 3900", result.launchDate.toString())
+        assertEquals("Sat Jan 01 00:00:00 BRST 2000", result.launchDate.toString())
     }
 
     @Test
@@ -108,7 +108,7 @@ class BookServiceTest {
         assertEquals("Title Test1",result.title)
         assertEquals("Author Test1",result.author)
         assertEquals(1.0,result.price)
-        assertEquals("Thu Feb 01 00:00:00 UTC 3900", result.launchDate.toString())
+        assertEquals("Sat Jan 01 00:00:00 BRST 2000", result.launchDate.toString())
 
     }
 
